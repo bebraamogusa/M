00cfd99bea75876c1f1742fa37d6b327ef3b86ef
micah <1569124799@qq.com>
2023-12-26 16:38:57 +0800

    update about

--- minty/Functions/About/About.cpp
+++ minty/Functions/About/About.cpp
@@ -7,15 +7,16 @@ namespace cheat {
         ImGui::Text(_("Minty version : %s"), "1.41");
         ImGui::Text("ImGui version: %s", ImGui::GetVersion());
         ImGui::Text("Design made with love by KittyKate :3");
-	ImGui::Text(_("Chinese localization by Amireux"));
 
         ImGui::SeparatorText("Contributors");
 
         ImGui::TextColored(ImVec4(235.0F / 255.0F, 64.0F / 255.0F, 52.0F / 255.0F, 1.0), "Owner: MintyGingy");
         ImGui::TextColored(ImVec4(219.0F / 255.0F, 57.0F / 255.0F, 219.0F / 255.0F, 1.0), "Co-founder: Moistcrafter");
-        ImGui::TextColored(ImVec4(57.0F / 255.0F, 68.0F / 255.0F, 219.0F / 255.0F, 1.0), "Contributors: EtoShinya, KittyKate, lilmayofuksu, USSY, akioukun, sad_akulka");
+        ImGui::TextColored(ImVec4(57.0F / 255.0F, 68.0F / 255.0F, 219.0F / 255.0F, 1.0), "Contributors: EtoShinya, KittyKate, lilmayofuksu, USSY, akioukun, sad_akulka,Micah");
         ImGui::TextColored(ImVec4(255, 0, 212, 255), "Donaters: Thomas_Heath, Blair, unmeinoshonen, USSY");
         ImGui::TextColored(ImVec4(0, 255, 179, 255), "Special thanks to family: Futchev, yarik0chka, keitaro_gg");
+        ImGui::TextColored(ImVec4(255, 233, 233, 22), "Translator: Micah,Amireux");
+
 
 	ImGui::Text("Minty Github: ");
 	TextURL("Link", "https://github.com/kindawindytoday/Minty-Releases", true, false);
